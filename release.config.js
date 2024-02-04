ad982606bfc07a41fd5c5e6165370f23d9dd0ab6
Benedikt Rötsch <benedikt@hashbite.net>
2024-02-05 00:03:59 +0100

    build: add semantic release

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ["main", { name: "canary", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
