bdaadbe6d4332c48ef2f23ab2a54df8d5dd2c776
Benedikt Rötsch <benedikt@hashbite.net>
2024-02-05 14:14:45 +0100

    feat: improve error messages and catching for cachedImport helper

--- gatsby-transformer-unified/src/utils.ts
+++ gatsby-transformer-unified/src/utils.ts
@@ -4,7 +4,19 @@ export async function cachedImport<Type>(packageName: string): Promise<Type> {
   if (importCache.has(packageName)) {
     return importCache.get(packageName) as Type;
   }
-  const importedPackage: Type = await import(packageName);
-  importCache.set(packageName, importedPackage);
-  return importedPackage;
+  try {
+    const importedPackage: Type = await import(packageName);
+    if (!importedPackage) {
+      throw new Error(
+        `Package ${packageName} got imported, but it is empty. This is probably a module resolution issue.`
+      );
+    }
+    importCache.set(packageName, importedPackage);
+    return importedPackage;
+  } catch (err) {
+    console.error(err);
+    throw new Error(
+      `Unable to import package ${packageName}. Check error in console.`
+    );
+  }
 }
